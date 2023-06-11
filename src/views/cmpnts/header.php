e9831665c81abf5ca66a1397883e651a281173f5
Keane Dalisay <keane.dalisay0118@gmail.com>
2023-06-12 07:55:59 +0800

    Add header cmpnt with nav links variable

---
+++ src/views/cmpnts/header.php
@@ -0,0 +1,9 @@
+<header>
+  <img src="assets/eXAM_logo.svg"
+    alt="The word 'exam' as the web app logo with the 'x' represented with a pencil and ruler" loading="lazy">
+  <nav>
+    <ul>
+      <?= $links ?>
+    </ul>
+  </nav>
+</header>
