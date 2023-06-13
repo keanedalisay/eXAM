5d4dd71179b8b527f568b5f4e5753ca73e99ffb7
Keane Dalisay <keane.dalisay0118@gmail.com>
2023-06-13 09:35:40 +0800

    Add uri routing as a class in the public index file

---
+++ public/index.php
@@ -0,0 +1,25 @@
+<?php
+spl_autoload_register(function (string $class) {
+  require_once str_ireplace(["app", "\\"], ["\\src", "/"], dirname(__FILE__, 2) . strtolower($class)) . ".php";
+});
+
+class Router
+{
+  private array $uris = [
+    "/login" => "\src\\views\login\index.php",
+  ];
+
+  public function route()
+  {
+    $request_uri = htmlspecialchars($_SERVER["REQUEST_URI"]);
+    if (array_key_exists($request_uri, $this->uris)) {
+      require_once str_ireplace(["\\"], ["/"], dirname(__FILE__, 2) . $this->uris[$request_uri]);
+      return;
+    }
+    header("HTTP/1.1 404 Not Found");
+  }
+}
+
+$router = new Router;
+$router->route();
+?>
