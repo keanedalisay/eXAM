c19a55ff1354d38ce0ed2deaae4d45466b3c8dc9
Keane Dalisay <keane.dalisay0118@gmail.com>
2023-06-13 15:55:48 +0800

    Link global CSS file in head cmpnt

--- src/views/cmpnts/head.php
+++ src/views/cmpnts/head.php
@@ -15,6 +15,7 @@ class Head extends Cmpnt
         <meta name="viewport" content="width=device-width,initial-scale=1.0">
         <meta name="author" content="Keane Dalisay">
         <meta name="application-name" content="eXAM">
+        <link href="/styles/global/global.css" rel="stylesheet">
         $this->tags
       </head>
     html;
