6b3709417cca0fd0e4700202735d2263e9c52618
Keane Dalisay <keane.dalisay0118@gmail.com>
2023-06-13 09:45:01 +0800

    Replace semi-colon with comma in meta tag content separation

--- src/views/cmpnts/head.php
+++ src/views/cmpnts/head.php
@@ -12,7 +12,7 @@ class Head extends Cmpnt
       
       <head>
         <meta charset='UTF-8'>
-        <meta name='viewport' content='width=device-width;initial-scale=1.0'>
+        <meta name='viewport' content='width=device-width,initial-scale=1.0'>
         <meta name='author' content='Keane Dalisay'>
         <meta name='application-name' content='eXAM'>
         $this->tags
