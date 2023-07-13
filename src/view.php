e23c04bec8dd1aa132d8fdfaa53edef54f04f31d
keanedalisay <keane.dalisay0118@gmail.com>
2023-07-13 10:44:14 +0800

    Refactor View class to use templates
    Adjust Model class to accept Database and Session classes

--- src/view.php
+++ src/view.php
@@ -1,42 +1,15 @@
 <?php
 namespace app;
 
-use app\cmpnts\{Head, Header, Main, Footer};
-
 class View
 {
-  protected Head $head;
-  protected Header $header;
-  protected Footer $footer;
-
-  protected Main $main;
-  public function __construct(Head $head, Header $header, Main $main, Footer $footer)
-  {
-    $this->head = $head;
-    $this->header = $header;
-    $this->main = $main;
-    $this->footer = $footer;
+  private Model $model;
+  public function __construct(Model $model){
+    $this->model = $model;
   }
 
-  public function render()
-  {
-    echo <<<html
-    <!DOCTYPE html>
-    <html lang="en">
-    html;
-    $this->head->render();
-    echo <<<html
-    
-      <body>
-    html;
-    $this->header->render();
-    $this->main->render();
-    $this->footer->render();
-    echo <<<html
-    
-      </body>
-    </html>
-    html;
+  public function render(){
+    require_once str_ireplace(["\\"], ["/"], dirname(__FILE__, 2) . $this->model->template);
   }
 }
 ?>
